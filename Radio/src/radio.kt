07a1445fc7544ce464d129731a1d84668c0a71f7
Francisco Rosal <francisco-rosal98@hotmail.com>
2018-07-07 18:00:33 -0600

    Avances

--- Radio/src/radio.kt
+++ Radio/src/radio.kt
@@ -13,18 +13,39 @@ fun main(args: Array<String>) {
     )
 
     do {
-        var salir: Boolean = false
+        var salir = false
         println(newRadio)
         println()
         println(newRadio.menu())
-        var action = readLine()!!
+        val action = readLine()!!
 
         if (newRadio.isTurnOn) {
             when (action) {
                 "1" -> newRadio.turnOff()
                 "2" -> newRadio.changeFrequency()
-                //"3" -> newRadio.upperStation()
-                //"4" -> newRadio.lowerStation()
+                "3", "4" -> {
+                    var strJumps: String?
+                    var jumps: Double
+                    var okJumps  = false
+                    do {
+                        println("De cuanto en cuanto? ")
+                        strJumps = readLine()
+                        if (strJumps != null) {
+                            try {
+                                jumps = strJumps.toDouble()
+                                when (action) {
+                                    "3" -> newRadio.upperStation(jumps)
+                                    "4" -> newRadio.lowerStation(jumps)
+                                }
+                                okJumps = true
+                            } catch (error: NumberFormatException) {
+                                println("Opcion invalida, vuelve a intentarlo")
+                                okJumps = false
+                            }
+                        }
+                    } while (!okJumps)
+
+                }
                 "5" -> newRadio.moreVolume()
                 "6" -> newRadio.lessVolume()
                 "7" -> {
